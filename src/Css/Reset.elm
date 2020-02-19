7420a14dd837a73b30317a8b2c667c19d630b93e
Yoshitaka Totsuka <yoshitaka.totsuka@siiibo.com>
2020-02-20 03:12:38 +0900

    Add EricMeyer

--- src/Css/Reset.elm
+++ src/Css/Reset.elm
@@ -1,11 +1,19 @@
 module Css.Reset exposing (html5Doctor, ress)
 
 import Css.Global exposing (global)
+import Css.Reset.EricMeyer as EricMeyer
 import Css.Reset.Html5Doctor as Html5Doctor
 import Css.Reset.Normalize as Normalize
 import Css.Reset.Ress as Ress
 import Html exposing (Html)
 import Html.Styled exposing (toUnstyled)
+
+
+ericMeyer : Html msg
+ericMeyer =
+    EricMeyer.ericMeyer
+        |> global
+        |> toUnstyled
 
 
 html5Doctor : Html msg
---
+++ src/Css/Reset/EricMeyer.elm
@@ -0,0 +1,140 @@
+module Css.Reset.EricMeyer exposing (ericMeyer)
+
+import Css exposing (baseline, block, border, borderCollapse, borderSpacing, collapse, display, fontSize, int, lineHeight, listStyle, margin, none, padding, pct, property, verticalAlign, zero)
+import Css.Global exposing (..)
+
+
+{-|
+
+    http://meyerweb.com/eric/tools/css/reset/
+    v2.0 | 20110126
+    License: none (public domain)
+
+-}
+ericMeyer : List Snippet
+ericMeyer =
+    [ each
+        [ html
+        , body
+        , div
+        , span
+        , typeSelector "applet"
+        , typeSelector "object"
+        , typeSelector "iframe"
+        , h1
+        , h2
+        , h3
+        , h4
+        , h5
+        , h6
+        , p
+        , blockquote
+        , pre
+        , a
+        , typeSelector "abbr"
+        , typeSelector "acronym"
+        , typeSelector "address"
+        , typeSelector "big"
+        , typeSelector "cite"
+        , code
+        , typeSelector "del"
+        , typeSelector "dfn"
+        , em
+        , img
+        , typeSelector "ins"
+        , typeSelector "kbd"
+        , q
+        , typeSelector "s"
+        , typeSelector "samp"
+        , small
+        , typeSelector "strike"
+        , strong
+        , typeSelector "sub"
+        , typeSelector "sup"
+        , typeSelector "tt"
+        , typeSelector "var"
+        , typeSelector "b"
+        , typeSelector "u"
+        , i
+        , typeSelector "center"
+        , dl
+        , dt
+        , dd
+        , ol
+        , ul
+        , li
+        , fieldset
+        , form
+        , label
+        , legend
+        , table
+        , caption
+        , tbody
+        , tfoot
+        , thead
+        , tr
+        , th
+        , td
+        , article
+        , aside
+        , canvas
+        , details
+        , typeSelector "embed"
+        , typeSelector "figure"
+        , typeSelector "figcaption"
+        , footer
+        , header
+        , typeSelector "hgroup"
+        , menu
+        , nav
+        , typeSelector "output"
+        , typeSelector "ruby"
+        , section
+        , summary
+        , time
+        , typeSelector "mark"
+        , audio
+        , video
+        ]
+        [ margin zero
+        , padding zero
+        , border zero
+        , fontSize (pct 100)
+        , property "font" "inherit"
+        , verticalAlign baseline
+        ]
+    , -- HTML5 display-role reset for older browsers
+      each
+        [ article
+        , aside
+        , details
+        , typeSelector "figcaption"
+        , typeSelector "figure"
+        , footer
+        , header
+        , typeSelector "hgroup"
+        , menu
+        , nav
+        , section
+        ]
+        [ display block ]
+    , body
+        [ lineHeight (int 1) ]
+    , each [ ol, ul ]
+        [ listStyle none ]
+    , each [ blockquote, q ]
+        [ property "quotes" "none" ]
+    , each
+        [ selector "blockquote::before"
+        , selector "blockquote::after"
+        , selector "q::before"
+        , selector "q::after"
+        ]
+        [ property "content" "''"
+        , property "content" "none"
+        ]
+    , typeSelector "table"
+        [ borderCollapse collapse
+        , borderSpacing zero
+        ]
+    ]
