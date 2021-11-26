0e133c452882eb7267a19a11dce852622e596bc4
Yoshitaka Totsuka <y047aka@gmail.com>
2021-11-27 03:35:44 +0900

    restore Ress v2

--- src/Css/Reset/Ress.elm
+++ src/Css/Reset/Ress.elm
@@ -1,12 +1,13 @@
-module Css.Reset.Ress exposing (snippets, v3)
+module Css.Reset.Ress exposing (snippets, v3, v2)
 
 {-|
 
-@docs snippets, v3
+@docs snippets, v3, v2
 
 -}
 
 import Css.Global exposing (Snippet)
+import Css.Reset.Ress.V2 as V2
 import Css.Reset.Ress.V3 as V3
 import Css.Reset.Ress.V4 as V4
 
@@ -23,3 +24,10 @@ snippets =
 v3 : List Snippet
 v3 =
     V3.snippets
+
+
+{-| v2.0.4
+-}
+v2 : List Snippet
+v2 =
+    V2.snippets
